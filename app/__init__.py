2bcaef90e05dcd035886ad8c1f8423ebc69bb874
SergBo77 <seman77@mail.ru>
2024-07-09 10:29:16 +0300

    add comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 # создаёт экземпляр класса Flask (переменную app)
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app = Flask(__name__)  # Создание экземпляра приложения Flask с указанием имени модуля (__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess' # Установка секретного ключа для защиты данных приложения
 
+# Импорт модуля routes из пакета app для обработки маршрутов
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,25 @@
+# импорт библиотек и приложения
 from flask import render_template, request, redirect, url_for
 from app import app
 
+# Список для хранения заполненных форм
 blanks = []
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
+    # Проверяем метод запроса
     if request.method == 'POST':
+        # Получаем данные из формы
         name = request.form.get('name')
         age = request.form.get('age')
         city = request.form.get('city')
         hobby = request.form.get('hobby')
+
+        # Проверяем, что все поля заполнены
         if name and age and city and hobby:
-           blanks.append({'name': name, 'age': age, 'city': city, 'hobby': hobby})
-           return redirect(url_for('index'))
+            # Добавляем данные в список
+            blanks.append({'name': name, 'age': age, 'city': city, 'hobby': hobby})
+            return redirect(url_for('index'))  # Перенаправляем на главную страницу после добавления данных
+
+    # Отображаем шаблон 'blank.html' с переданным списком заполненных форм
     return render_template('blank.html', blanks=blanks)
